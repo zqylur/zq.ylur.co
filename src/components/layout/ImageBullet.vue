9f4ec66b7269211e658ba430e72efeddd4372774
Zach Weiler <zach.m.weiler@gmail.com>
2018-12-30 11:45:06 -0800

    Add first parts of resume
    Adds initial skills for resume. Adds ImageBullet component for logo +
    text lockup for skills.

---
+++ src/components/layout/ImageBullet.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img :src="require(`../../assets/${logo}.svg`)" /> <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageBullet',
+  props: { text: String(''), logo: String('') },
+};
+</script>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  align-items: center;
+  & > * {
+    padding: 5px;
+    line-height: 1.5em;
+    flex-basis: content;
+  }
+}
+img {
+  height: 24px;
+  width: 24px;
+}
+
+span {
+  font-size: 1.5em;
+}
+</style>
--- src/components/routes/Resume.vue
+++ src/components/routes/Resume.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>Resume</div>
+  <div class="resume">
+    <ul>
+      <li>
+        <ImageBullet
+          text="Javascript"
+          logo="JS"
+        />
+      </li>
+      <li>
+        <ImageBullet
+          text="Typescript"
+          logo="TS"
+        />
+      </li>
+      <ImageBullet
+        text="webpack"
+        logo="webpack"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
+import ImageBullet from '../layout/ImageBullet.vue';
+
 export default {
   name: 'Resume',
+  components: {
+    ImageBullet,
+  },
 };
 </script>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+}
+</style>
